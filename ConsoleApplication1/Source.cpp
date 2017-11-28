803e529237696c1d6b4f4db3e8b16f9605ca6422
Varvolk <33603027+Varvolk@users.noreply.github.com>
2017-11-28 19:43:14 +0200

    v1.0.1

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -24,11 +24,15 @@ void funcbild(Ptr<BRISK> &detectorBRISK, Mat img1, Mat img2) {
 	Mat imgMatch= img1.clone(), imgMatch2 = img2.clone();
 	Mat imgFinal, imgMatch3;
    cout << "First img - number of keypoints: "<< keypointsImageTemple.size() <<endl;
-	for(int n=0;n<keypointsImageTemple.size();++n)
-	circle(imgMatch, keypointsImageTemple[n].pt,2,Scalar(0, 0, 255), -1, 8);
+   for (int n = 0; n < keypointsImageTemple.size(); ++n) {
+	   circle(imgMatch, keypointsImageTemple[n].pt, 4, Scalar(0, 0, 0), -1, 8);
+	   circle(imgMatch, keypointsImageTemple[n].pt, 2, Scalar(0, 0, 255), -1, 8);
+   }
 	cout << "Second img - number of keypoints: " << keypointsImage.size() << endl;
-	for (int n = 0; n<keypointsImage.size(); ++n)
+	for (int n = 0; n < keypointsImage.size(); ++n) {
+		circle(imgMatch2, keypointsImage[n].pt, 4, Scalar(0, 0, 0), -1, 8);
 		circle(imgMatch2, keypointsImage[n].pt, 2, Scalar(0, 0, 255), -1, 8);
+	}
 	double persentoftrueKeypoint= keypointsImage.size();
 
 
@@ -44,10 +48,18 @@ void funcbild(Ptr<BRISK> &detectorBRISK, Mat img1, Mat img2) {
 	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 	drawMatches(img1, keypointsImageTemple, img2, keypointsImage, matches, imgMatch3);
 	imwrite("matches.jpeg", imgMatch3);
-	
-	
+	std::cout << endl <<"Size of 1-st img Descriptor:   "<< descriptorsImageTemple.size()<<endl;
+	std::cout << endl <<"Size of 2-nd img Descriptor:   " << descriptorsImage.size() << endl;
+	std::cout << "first descriptors of the first image:_"<<endl;
+
+
+	for (int i = 0; i < descriptorsImageTemple.cols; ++i) {
+		if (i % 8 == 0)cout << endl;
+		std::cout << descriptorsImageTemple.row(0).col(i) << " ";
+		
+	}
 	////////////////////////////////////////
-	std::vector<Point2f> obj;
+/*	std::vector<Point2f> obj;
 	std::vector<Point2f> scene;
 	for (int i = 0; i < matches.size(); i++)
 	{
@@ -70,19 +82,19 @@ void funcbild(Ptr<BRISK> &detectorBRISK, Mat img1, Mat img2) {
 	line(imgMatch3, scene_corners[2] + Point2f(img1.cols, 0), scene_corners[3] + Point2f(img1.cols, 0), Scalar(0, 255, 0), 4);
 	line(imgMatch3, scene_corners[3] + Point2f(img1.cols, 0), scene_corners[0] + Point2f(img1.cols, 0), Scalar(0, 255, 0), 4);
 	imwrite("matches3.jpeg", imgMatch);
-
+	*/
 }
 
 
 
 int main() {
-	int  	thresh = 30;
-	int  	octaves = 3;
-	float  	patternScale = 1.0f;
+	int  	thresh = 60;
+	int  	octaves = 0;
+	float  	patternScale = 0.5f;
 	Ptr<BRISK> detectorBRISK = BRISK::create(thresh, octaves, patternScale);
 	cout << "START " << endl;
-	Mat img1 = imread("im3.jpg");
-	Mat img2 = imread("im3.jpg");
+	Mat img1 = imread("im1.jpg");
+	Mat img2 = imread("im2.jpg");
 	funcbild(detectorBRISK,img1, img2);
 	waitKey(0);
 	return 0;
